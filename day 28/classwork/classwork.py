64ac1c299f1adf27b334b618519a4f8427f64c27
nika-kotrikadze <nikakotrikadze1239@gmail.com>
2024-08-13 14:06:27 +0400

    second commit

---
+++ day 28/classwork/classwork.py
@@ -0,0 +1,78 @@
+#1
+#for i in range(1,21):
+    #print(i)
+
+
+#2
+#num1= int(input("enter number"))
+
+#num2= int(input("enter number"))
+
+#for i in range(0,20):
+    #print(num1,num2)
+
+#3
+#for i in range(50,101):
+    #print(i)
+
+
+#4
+#for i in range(100,49,-1):
+    #print(i)
+
+#5
+#num3=0
+#for i in range(0,51):
+    #num1+=i
+    #print(i)
+
+#6
+#num4=int(input("enter number"))
+#num5=0
+#for i in range(num2,num1):
+    #print(i)
+
+#7
+#age1=int(input("enter your age"))
+#for i in range(0, age1):
+    #print(i)
+
+#8
+#num6=int(input("enter your number"))
+#num7=int(input("enter your number"))
+p#rint(num6+num7)
+#print(num7-num6)
+#print(num6*num7)
+#print(num7/num6)
+#print(num7//num6)
+#print(num6%num7)
+
+#9
+#age2=int(input("enter your age"))
+#if age2 <18==18:
+    #print("you are allowed")
+#else:
+    #print("you are not allowed")
+
+#10
+#print(1223232<2)#falce
+#print(8232>9991133)
+#print(1133<=1223)#true
+#print(993338>=434244442)#falce
+#print(11222211==11222211)#true
+#print(21112323!=3234355)#falce
+
+#11
+#num8=int(input("enter any integer"))
+#float1=float(input("enter any float"))
+#num8=int
+#float1=float
+
+#12
+#print(int(12))
+#print(str("hello world"))
+#print(float(1.2))
+
+
+
+
---
+++ day 28/homework/homework.py
@@ -0,0 +1,57 @@
+#1
+#week_day=int(input("enter number 1 to 6: "))
+#if week_day==1:
+    #print("monday")
+#elif week_day==2:
+    #print("thuesday")
+#elif week_day==3:
+    #print("wednesday")
+#elif week_day==4:
+    #print("thursday")
+#elif week_day==5:
+    #print("saturday")
+#elif week_day==6:
+    #print("sunday")
+#else:
+    #print("error")
+
+
+#2
+#num = int (input ("Enter any number 1 to 10: ")) 
+
+#while num >= 10:
+        #print("error")
+        #num = int (input ("enter any number 1 to 10: "))
+
+#while (num % 2) != 0:
+
+              #print ("The number is not even")
+              #num = int (input ("Enter any number 1 to 10: "))
+
+#else:
+    #print ("The provided number is even")
+
+
+#3
+#age = int (input ("enter your age: "))
+#if age < 5:
+    #print("error")
+    #age = int (input ("enter your age: "))
+#elif age >= 5 <= 12:
+    #print ("you are child")
+    #age = int (input ("enter your age: "))
+#elif age > 12 < 18:
+    #print("you are tennager")
+    #age = int (input ("enter your age: "))
+#elif age > 18:
+    #print("you are adult")
+    #age = int (input ("enter your age: "))
+
+
+#4
+#budget = int (input ("enter your budget: "))
+#iphone = 1500
+#if budget < 1500:
+    #print("you can't buy this item")
+#elif budget >= 1500:
+    #print("you can buy this item")
